b5302ab89e8de7355cbd8b599441480337b2e542
Richard Jackson <jacksonr16@cardiff.ac.uk>
2022-02-26 22:47:35 +0000

    3-3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Fragment, Component } from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
-import UserItem from './components/users/UserItem';
+import Users from './components/users/Users';
 
 class App extends Component {
 	// Lifecycle method
@@ -18,7 +18,7 @@ class App extends Component {
 				) : (
 					<h1>Hello {showName && name + '!'}</h1>
 				)}
-				<UserItem />
+				<Users />
 			</Fragment>
 		);
 	}
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -2,16 +2,12 @@ import React, { Component } from 'react';
 
 class UserItem extends Component {
 	// Defining state
-	state = {
-		name: 'Rich',
-	};
-
 	render() {
 		// Destructure state (get variables)
-		const { name } = this.state;
+		const { login } = this.props.user;
 		return (
 			<div className='card'>
-				<h1 className='NameOfPerson'>{name}</h1>
+				<h1 className='NameOfPerson'>Hi: {login}</h1>
 			</div>
 		);
 	}
