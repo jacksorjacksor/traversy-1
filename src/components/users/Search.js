cc560f1d0a346d531a3e0e8e14747ed842a8a545
Richard Jackson <jacksonr16@cardiff.ac.uk>
2022-02-27 21:15:54 +0000

    5-2

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,53 +1,49 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-class Search extends Component {
-	state = {
-		text: '',
-	};
-
-	static propTypes = {
-		searchUsers: PropTypes.func.isRequired,
-		clearUsers: PropTypes.func.isRequired,
-		showClear: PropTypes.bool.isRequired,
-		setAlert: PropTypes.func.isRequired,
-	};
+const Search = ({ searchUsers, showClear, clearUsers, setAlert }) => {
+	// useState hook
+	const [text, setText] = useState('');
 
 	// Takes the name of whatever fired the onChange event, then stores the value in state
-	onChange = (e) => this.setState({ [e.target.name]: e.target.value });
+	const onChange = (e) => setText(e.target.value);
 
-	onSubmit = (e) => {
+	const onSubmit = (e) => {
 		e.preventDefault();
 
 		// Alert if empty:
-		if (this.state.text === '') {
-			this.props.setAlert('Please enter something', 'light');
+		if (text === '') {
+			setAlert('Please enter something', 'light');
 		} else {
-			this.props.searchUsers(this.state.text);
-			this.setState({ text: '' });
+			searchUsers(text);
+			setText('');
 		}
 	};
 
-	render() {
-		// Destructuring
-		const { showClear, clearUsers } = this.props;
-		return (
-			<div>
-				<form onSubmit={this.onSubmit} action=''>
-					<input
-						type='text'
-						name='text'
-						id=''
-						value={this.state.text}
-						onChange={this.onChange}
-					/>
-					<input type='submit' value='Search' />
-				</form>
-				{/* Conditional: if showClear then do the thing */}
-				{showClear && <button onClick={clearUsers}>Clear</button>}
-			</div>
-		);
-	}
-}
+	// Destructuring
+	return (
+		<div>
+			<form onSubmit={onSubmit} action=''>
+				<input
+					type='text'
+					name='text'
+					id=''
+					value={text}
+					onChange={onChange}
+				/>
+				<input type='submit' value='Search' />
+			</form>
+			{/* Conditional: if showClear then do the thing */}
+			{showClear && <button onClick={clearUsers}>Clear</button>}
+		</div>
+	);
+};
+
+Search.propTypes = {
+	searchUsers: PropTypes.func.isRequired,
+	clearUsers: PropTypes.func.isRequired,
+	showClear: PropTypes.bool.isRequired,
+	setAlert: PropTypes.func.isRequired,
+};
 
 export default Search;
